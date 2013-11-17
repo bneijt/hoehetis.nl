a959ecaedf2a26e3593fb83d782b3449d7b620e7
A. Bram Neijt <bneijt@gmail.com>
2013-11-17 22:58:59 +0100

    Add ulimit

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export LC_CTYPE=en_US.utf-8
 export PATH=$PATH:/bin:/usr/bin
-
 set -e
+ulimit -v 102400
 cd "`dirname "$0"`"
 . virtualenv/bin/activate
 python src/gen.py 2>&1 | tee run.log
