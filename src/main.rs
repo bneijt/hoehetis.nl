831e4e1ae802867bd7711f43990c5c0bfd49e24f
Bram Neijt <bram@neijt.nl>
2023-10-02 20:46:50 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,15 @@
 use duckdb::*;
 use feed_rs::parser;
 use reqwest::Client;
-use serde::{Deserialize, Serialize};
 use soup::Soup;
 use tokio;
 
-#[derive(Debug, Deserialize, Serialize)]
-struct RssItem {
-    guid: String,
-    title: String,
-    description: String,
-}
-
-#[tokio::main]
-async fn main() {
+async fn update_items(connection: Connection) -> () {
     // Step 1: Download the RSS feed
     let feed_url = "https://feeds.nos.nl/nosnieuwsalgemeen";
     let response = Client::new().get(feed_url).send().await.unwrap();
     let body = response.text().await.unwrap();
     let channel = parser::parse(body.as_bytes()).unwrap();
-
-    // Open and initialize database
-    let connection = Connection::open("data/nos_algemeen.duckdb").expect("Failed to open database");
 
     connection
         .execute(
@@ -37,21 +25,26 @@ async fn main() {
     for item in channel.entries {
         //Extract guid, title and description from entry
         // dbg!(item);
-        let rss_item = RssItem {
-            guid: item.id,
-            title: item.title.unwrap().content,
-            description: Soup::new(
-                // format!("<body>{}</body>", item.summary.unwrap().content).as_str(),
-                item.summary.unwrap().content.as_str(),
-            )
-            .text(),
-        };
+        let guid = item.id;
+        let title = item.title.unwrap().content;
+        let description = Soup::new(
+            // format!("<body>{}</body>", item.summary.unwrap().content).as_str(),
+            item.summary.unwrap().content.as_str(),
+        )
+        .text();
         connection
             .execute(
                 "insert or ignore into news_items values (?,?,?)",
-                params![rss_item.guid, rss_item.title, rss_item.description],
+                params![guid, title, description],
             )
             .expect("Failed to insert item");
     }
+}
+
+#[tokio::main]
+async fn main() {
+    // Open and initialize database
+    let connection = Connection::open("data/nos_algemeen.duckdb").expect("Failed to open database");
+    update_items(connection).await;
     ()
 }
