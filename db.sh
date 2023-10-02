89a11bbea51085d1a38880fcfa16da613ac791b6
Bram Neijt <bram@neijt.nl>
2023-10-02 20:13:11 +0200

    Use duckdb

--- db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#--hostname=roach-single   --net=roachnet 
-docker run -it  --env COCKROACH_DATABASE=nos_algemeen --env COCKROACH_USER=frank \
- --env COCKROACH_PASSWORD=frank  -p 26257:26257   -p 8080:8080 \
- -v "roach-single:/cockroach/cockroach-data" cockroachdb/cockroach start-single-node --insecure --http-addr=0.0.0.0:8080
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,10 @@
+use duckdb::*;
 use feed_rs::parser;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
-use surrealdb::engine::remote::ws::Ws;
-// use surrealdb::opt::auth::Root;
-use surrealdb::sql::Thing;
-use surrealdb::Surreal;
 use tokio;
 
-#[derive(Debug, Deserialize)]
-struct Record {
-    #[allow(dead_code)]
-    id: Thing,
-}
-
-
-#[derive(Debug, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 struct RssItem {
     guid: String,
     title: String,
@@ -22,22 +12,23 @@ struct RssItem {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() {
     // Step 1: Download the RSS feed
     let feed_url = "https://feeds.nos.nl/nosnieuwsalgemeen";
-    let response = Client::new().get(feed_url).send().await?;
-    let body = response.text().await?;
+    let response = Client::new().get(feed_url).send().await.unwrap();
+    let body = response.text().await.unwrap();
     let channel = parser::parse(body.as_bytes()).unwrap();
 
-    // Step 2: Extract data and write to SurrealDB
-    let surreal_db_url = "localhost:8000"; // Replace with your SurrealDB URL
-    let db = Surreal::new::<Ws>(surreal_db_url).await?;
-    // db.signin(Root {
-    //     username: "root",
-    //     password: "root",
-    // })
-    // .await?;
-    db.use_ns("hoehetis").use_db("nos_algemeen").await?;
+    // Open and initialize database
+    let connection =
+        Connection::open("data/nos_algemeen.duckdb").expect("Failed to open database");
+
+    connection
+        .execute(
+            "CREATE TABLE news_items (guid VARCHAR, title VARCHAR, description VARCHAR)",
+            params![],
+        )
+        .expect("Failed to create table");
 
     for item in channel.entries {
         //Extract guid, title and description from entry
@@ -47,10 +38,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             title: item.title.unwrap().content,
             description: item.summary.unwrap().content,
         };
-
-        let created: Vec<Record> = db.create("items").content(rss_item).await?;
-        dbg!(created);
-    }
-
-    Ok(())
+        connection
+            .execute(
+                "INSERT INTO news_items VALUES (?,?,?)",
+                params![rss_item.guid, rss_item.title, rss_item.description]
+            )
+            .expect("Failed to insert item");
+    };
+    ()
 }
