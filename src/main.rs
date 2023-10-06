11dfde0e4d866b8991ec27c68bc1b2d5a2fc6a4c
Bram Neijt <bram@neijt.nl>
2023-10-06 11:55:29 +0200

    Ugly but works

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use duckdb::*;
 use feed_rs::parser;
 use reqwest;
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 use soup::Soup;
 use tokio;
 mod db;
@@ -15,13 +14,21 @@ use canyon_sql::macros::canyon_entity;
 
 #[derive(Serialize, Deserialize)]
 struct Emotions {
+    #[serde(default)]
     anger: f32,
+    #[serde(default)]
     anticipation: f32,
+    #[serde(default)]
     joy: f32,
+    #[serde(default)]
     trust: f32,
+    #[serde(default)]
     fear: f32,
+    #[serde(default)]
     surprise: f32,
+    #[serde(default)]
     sadness: f32,
+    #[serde(default)]
     disgust: f32,
 }
 
@@ -42,7 +49,7 @@ async fn update_items(connection: &Connection) -> () {
     let response = reqwest::Client::new().get(feed_url).send().await.unwrap();
     let body = response.text().await.unwrap();
     let channel = parser::parse(body.as_bytes()).unwrap();
-    
+
     for item in channel.entries {
         //Extract guid, title and description from entry
         // dbg!(item);
@@ -67,16 +74,10 @@ async fn update_items(connection: &Connection) -> () {
     }
 }
 
-#[tokio::main]
-async fn main() {
-
-    // Open and initialize database
-    let connection = Connection::open("data/nos_algemeen.duckdb").expect("Failed to open database");
-    db::create_tables(&connection).await;
-
-    update_items(&connection).await;
-
-    let guid = "1";
+/// Load the given guid content into emotions from chat_gpt
+async fn load_emotions(connection: &Connection, guid: &str) -> () {
+    let mut stmt = connection.prepare("select description from news_items where guid = ?").unwrap();
+    let news_item: String = stmt.query(params![guid]).unwrap().next().unwrap().unwrap().get(0).unwrap();
     let client = async_openai::Client::new();
 
     let request = CreateChatCompletionRequestArgs::default()
@@ -90,7 +91,7 @@ async fn main() {
                 .unwrap(),
             ChatCompletionRequestMessageArgs::default()
                 .role(Role::User)
-                .content(r###"Er was nog weinig twijfel over, maar nu is er het definitieve bewijs dat prins Bernhard lid was van de NSDAP, de partij van Adolf Hitler. De originele NSDAP-lidmaatschapskaart is in het archief van de prins gevonden.In zijn boek De Achterblijvers, dat deze woensdag verschijnt, onthult Flip Maarschalkerweerd dat hij de originele NSDAP-lidmaatschapskaart van de prins heeft gevonden, schrijft NRC. Maarschalkerweerd is oud-directeur van het Koninklijk Huisarchief. Hij vond de kaart op paleis Soestdijk in het privéarchief van de prins dat hij na diens overlijden in 2004 moest inventariseren.Tot zijn dood ontkendPrins Bernhard heeft tot aan zijn dood ontkend dat hij lid was van de nazipartij. Ook toen hij met eerder bewijs werd geconfronteerd. In 1996 meldden historici al dat ze een kopie van zijn lidmaatschapskaart gevonden hadden in de VS. Ook werd er correspondentie over de opzegging van lidmaatschap uit 1936 gevonden. In dat jaar verloofde hij zich met Juliana. Bernhard gaf alleen toe dat hij een tijd lid was geweest van een afdeling van de SS.Prins BernardPrins Bernhard (1911-2004), geboren in Duitsland, was de man van prinses Juliana, de vader van prinses Beatrix en opa van koning Willem-Alexander. Tijdens de oorlog vluchtte hij met Juliana naar Londen. Aan het einde van de oorlog werd hij door koningin Wilhelmina benoemd tot bevelhebber van de Binnenlandse Strijdkrachten, waar verschillende verzetsgroepen onder vielen.In een serie vraaggesprekken die hij voor zijn dood had met de Volkskrant zei de prins nog: "Ik kan met de hand op de Bijbel verklaren: ik was nooit een nazi. Ik heb nooit het partijlidmaatschap betaald, ik heb nooit een lidmaatschapskaart gehad."'Spectaculaire vondst'Historica en schrijfster Annejet van der Zijl, die het boek al heeft gelezen, noemt de vondst "spectaculair". "Echt mooi dat 100 procent de historische waarheid is komen bovendrijven. Dat is een pluim waard voor Maarschalkerweerd en voor de koninklijke familie, die toelaat dat dit openbaar wordt", zegt ze in het NOS Radio 1 Journaal. Van der Zijl schreef in 2010 een boek over Bernhard, waarin ze onthulde dat hij lid was geweest van een nationaalsocialistische studentenvereniging. Ook stond er op de lidmaatschapskaart van die vereniging een aantekening dat hij lid was van de NSDAP.Het verbaast Van der Zijl dat Bernhard de lidmaatschapskaart niet heeft verbrand. "Er zijn genoeg open haarden op Soestdijk, lijkt mij. Ik kan niet anders concluderen dan dat hij toch wel hechtte aan die periode in zijn leven", zegt ze. "Hij is ook altijd heel trouw gebleven aan vrienden uit die tijd die overtuigd nazi's waren en heeft altijd de vriendschap onderhouden."Volgens de historica heeft Bernhard nooit ingezien "hoe verwerpelijk het was". "Maar hij kon er ook niet mee naar buiten komen, want hij stond elk jaar weer op de Dam met allemaal leiders van Joodse belangenverenigingen vroom een krans te leggen voor de Dodenherdenking. En hij was de leider van het verzet. Het is een heel gespleten leven wat deze man heeft gehad."###)
+                .content(news_item)
                 .build()
                 .unwrap(),
         ])
@@ -120,6 +121,29 @@ async fn main() {
             emotions.disgust],
         )
         .expect(format!("Failed to insert response for {guid}").as_str());
+    ()
+}
 
+#[tokio::main]
+async fn main() {
+    // Open and initialize database
+    let connection = Connection::open("data/nos_algemeen.duckdb").expect("Failed to open database");
+    db::create_tables(&connection).await;
+
+    update_items(&connection).await;
+
+    // Fetch guids that have not run through chatgpt yet
+    let mut stmt = connection.prepare("select guid from news_items anti join chat_responses using (guid)").unwrap();
+    let missing_chats_iter = stmt.query_map([], |row| {
+        let v:String = row.get(0).unwrap();
+        Ok(v)
+    }).unwrap();
+
+
+    for guid_result in missing_chats_iter {
+        let guid = guid_result.unwrap();
+        println!("Loading guid {}", guid);
+        load_emotions(&connection, &guid).await;
+    }
     ()
 }
