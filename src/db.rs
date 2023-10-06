fcbdcb496d37c027e66a31014cde59e67d59b4e2
Bram Neijt <bram@neijt.nl>
2023-10-06 10:26:19 +0200

    Emotion tables

---
+++ src/db.rs
@@ -0,0 +1,42 @@
+use duckdb::*;
+
+pub async fn create_tables(connection: &duckdb::Connection) -> () {
+    connection
+        .execute(
+            "create table  if not exists news_items(
+                guid varchar primary key,
+                published_date date,
+                title varchar,
+                description varchar
+            )",
+            params![],
+        )
+        .expect("Failed to create news_items table");
+    connection
+        .execute(
+            "create table  if not exists chat_responses(
+                    guid varchar primary key,
+                    response_content varchar
+                )",
+            params![],
+        )
+        .expect("Failed to create chat_responses table");
+    connection
+        .execute(
+            "create table  if not exists emotions(
+                    guid varchar primary key,
+                    anger float,
+                    anticipation float,
+                    joy float,
+                    trust float,
+                    fear float,
+                    surprise float,
+                    sadness float,
+                    disgust float
+                )",
+            params![],
+        )
+        .expect("Failed to create emotions table");
+
+    ()
+}
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,36 @@ use async_openai::types::{
 use duckdb::*;
 use feed_rs::parser;
 use reqwest;
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use soup::Soup;
 use tokio;
+mod db;
+
+use canyon_sql::macros::canyon_entity;
+
+#[derive(Serialize, Deserialize)]
+struct Emotions {
+    anger: f32,
+    anticipation: f32,
+    joy: f32,
+    trust: f32,
+    fear: f32,
+    surprise: f32,
+    sadness: f32,
+    disgust: f32,
+}
+
+#[canyon_entity]
+pub struct League {
+    #[primary_key]
+    pub id: i32,
+    pub ext_id: i64,
+    pub slug: String,
+    pub name: String,
+    pub region: String,
+    pub image_url: String,
+}
 
 async fn update_items(connection: &Connection) -> () {
     // Step 1: Download the RSS feed
@@ -14,19 +42,7 @@ async fn update_items(connection: &Connection) -> () {
     let response = reqwest::Client::new().get(feed_url).send().await.unwrap();
     let body = response.text().await.unwrap();
     let channel = parser::parse(body.as_bytes()).unwrap();
-
-    connection
-        .execute(
-            "create table  if not exists news_items(
-                    guid varchar primary key,
-                    published_date date,
-                    title varchar,
-                    description varchar
-                )",
-            params![],
-        )
-        .expect("Failed to create table");
-
+    
     for item in channel.entries {
         //Extract guid, title and description from entry
         // dbg!(item);
@@ -53,26 +69,14 @@ async fn update_items(connection: &Connection) -> () {
 
 #[tokio::main]
 async fn main() {
+
     // Open and initialize database
     let connection = Connection::open("data/nos_algemeen.duckdb").expect("Failed to open database");
-    update_items(&connection).await;
-    connection
-        .execute(
-            "create table  if not exists emotions(
-                    guid varchar primary key,
-                    anger float,
-                    anticipation float,
-                    joy float,
-                    trust float,
-                    fear float,
-                    surprise float,
-                    sadness float,
-                    disgust float
-                )",
-            params![],
-        )
-        .expect("Failed to create table");
+    db::create_tables(&connection).await;
 
+    update_items(&connection).await;
+
+    let guid = "1";
     let client = async_openai::Client::new();
 
     let request = CreateChatCompletionRequestArgs::default()
@@ -93,56 +97,29 @@ async fn main() {
         .build()
         .unwrap();
     let response = client.chat().create(request).await.unwrap();
-    println!("\nResponse:\n");
-    for choice in response.choices {
-        println!(
-            "{}: Role: {}  Content: {:?}",
-            choice.index, choice.message.role, choice.message.content
-        );
-    }
-
-    // {
-    //     "id": "chatcmpl-123",
-    //     "object": "chat.completion",
-    //     "created": 1677652288,
-    //     "model": "gpt-3.5-turbo-0613",
-    //     "choices": [{
-    //       "index": 0,
-    //       "message": {
-    //         "role": "assistant",
-    //         "content": "\n\nHello there, how may I assist you today?",
-    //       },
-    //       "finish_reason": "stop"
-    //     }],
-    //     "usage": {
-    //       "prompt_tokens": 9,
-    //       "completion_tokens": 12,
-    //       "total_tokens": 21
-    //     }
-    //   }
-
-    // let api_key = "YOUR_API_KEY"; // Replace with your OpenAI API key
-    // let prompt = "how are you?";
-    // let endpoint = "https://api.openai.com/v1/chat/completions";
-
-    // let client = reqwest::Client::new();
-    // let response = client
-    //     .post(endpoint)
-    //     .header("Authorization", format!("Bearer {}", api_key))
-    //     .json(&json!({
-    //         "model": "gpt-3.5-turbo",
-    //         "messages": [
-    //             {"role": "system", "content": "You are a helpful assistant."},
-    //             {"role": "user", "content": prompt}
-    //         ]
-    //     }))
-    //     .send()
-    //     .await?;
-
-    // let json_response: Value = response.json().await?;
-    // let choices = &json_response["choices"][0]["message"]["content"];
-
-    // println!("Response: {}", choices);
+    let choice = response.choices.first().unwrap().clone();
+    let json_response = choice.message.content.unwrap();
+    connection
+        .execute(
+            "insert or ignore into chat_responses(guid, response_content) values (?,?)",
+            params![guid, json_response],
+        )
+        .expect(format!("Failed to insert response for {guid}").as_str());
+    // Parse response message as json and insert into database
+    let emotions: Emotions = serde_json::from_str(&json_response).unwrap();
+    connection
+        .execute(
+            "insert or ignore into emotions(guid, anger, anticipation, joy, trust, fear, surprise, sadness, disgust) values (?,?,?,?,?,?,?,?,?)",
+            params![guid, emotions.anger,
+            emotions.anticipation,
+            emotions.joy,
+            emotions.trust,
+            emotions.fear,
+            emotions.surprise,
+            emotions.sadness,
+            emotions.disgust],
+        )
+        .expect(format!("Failed to insert response for {guid}").as_str());
 
     ()
 }
