6eb1fb12091647a951f14a00762ada8546778996
Bram Neijt <bram@neijt.nl>
2023-11-10 13:39:55 +0100

    Add run script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+if [[ -z "${OPENAI_API_KEY}" ]]; then
+  echo "OPENAI_API_KEY is not set"
+  exit 1
+fi
+
+TODAY=$(date +"%Y-%m-%d")
+
+docker-compose up
+
+git commit -am "Update $TODAY"
+git push
